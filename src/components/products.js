22a732c487de534eb4634f2617e444a91cd50ed9
sanusijen <89282336+sanusijen@users.noreply.github.com>
2026-05-05 08:00:48 -0700

    Remove unnecessary newline in products.js

--- src/components/products.js
+++ src/components/products.js
@@ -2,7 +2,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-
 const cardStyle = {
   border: 'none',
   borderRadius: '12px',
