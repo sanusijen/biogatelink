73d25b3ac0425ebc741aad651e9e42db2621d257
sanusijen <89282336+sanusijen@users.noreply.github.com>
2026-05-12 14:34:16 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -26,7 +26,8 @@ function AppFooter() {
       <div className="copyright">&copy; 2025 Biogate. All Right Reserved.</div>
       <div className="socials">
         <ul>
-          <li><a href="https://www.facebook.com"><i className="fab fa-facebook-f"></i></a></li>
+          <li><a
+          href="https://www.facebook.com/share/1TwUHEAsiy/"><i className="fab fa-facebook-f"></i></a></li>
           <li><a href="https://www.twitter.com"><i className="fab fa-twitter"></i></a></li>
           <li><a href="https://www.linkedin.com"><i className="fab fa-linkedin-in"></i></a></li>
         </ul>
